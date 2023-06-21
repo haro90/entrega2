33066e6767623c847defecb6af4ffbf979bcb3fb
Carlos Haro <haro.arq@gmail.com>
2023-06-21 10:52:39 -0600

    final

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
 const http = require("http");
-const io = require("socket.io");
+const { Server } = require("socket.io");
 const ProductManager = require("../ProductManager");
 
 const productManagerInstance = new ProductManager();
@@ -9,7 +9,7 @@ const productManagerInstance = new ProductManager();
 const app = express();
 const port = 8080;
 const server = http.createServer(app);
-const socketIO = io(server);
+const io = new Server(server);
 
 app.use(express.json());
 app.use(express.static("public"));
@@ -34,22 +34,28 @@ app.use("/api/products", productsRouter);
 app.use("/api/cart", cartRouter);
 app.use("/", viewsRouter);
 
-socketIO.on("connection", (socket) => {
+io.on("connection", (socket) => {
   console.log("Nuevo cliente conectado");
 
-  socket.on("addProduct", (product) => {
-    console.log("Producto agregado:", product);
-    const newProduct = productManagerInstance.addProduct(product);
-    socketIO.emit("productAdded", newProduct);
+  socket.on("productAdded", function (product) {
+    if (product && product.title) {
+      product.id = generateProductId();
+      productManagerInstance.addProduct(product);
+      io.emit("productAdded", product);
+      io.emit("updateProductList", productManagerInstance.getProducts());
+    }
   });
 
   socket.on("deleteProduct", (productId) => {
     console.log("Producto eliminado:", productId);
     const deletedProductId = productManagerInstance.deleteProduct(productId);
-    socketIO.emit("productDeleted", deletedProductId);
+    io.emit("productDeleted", deletedProductId);
+    io.emit("updateProductList", productManagerInstance.getProducts());
   });
 });
 
 server.listen(port, () => {
   console.log(`Servidor escuchando en el puerto ${port}`);
 });
+
+const generateProductId = productManagerInstance.generateProductId;
